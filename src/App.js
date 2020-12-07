771118ac8585fac5d6ce0705b0463e8099d9f925
rudani-c <rudani.c@gmail.com>
2020-12-07 18:36:45 -0500

    initial setup

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+import Main from './components/main'
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <div className="App">
+      <Main />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/main.js
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react";
+
+const Main = () => {
+
+
+
+  return (
+    <>
+        <h1>Restaurants</h1>
+    </>
+  );
+};
+
+export default Main;
