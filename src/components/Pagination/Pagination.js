fa35bcf8b7aba6addd8071597d2cdaece59a4f76
rudani-c <rudani.c@gmail.com>
2020-12-10 05:54:05 -0500

    pagination

---
+++ src/components/Pagination/Pagination.js
@@ -0,0 +1,29 @@
+import './pagination.css';
+
+import React from 'react';
+
+export const Pagination = ({ dataPerPage, totaldata, paginate, currentPage }) => {
+    const pageNumbers = [];
+
+    for (let i = 1; i <= Math.ceil(totaldata / dataPerPage); i++) {
+        pageNumbers.push(i);
+    }
+
+    return (
+        <nav>
+            <div className="pagination">
+                <a href="#" onClick={() => paginate(1)}>&laquo;</a>
+                {pageNumbers.map((number, index) => {
+                    const applyStyle = [];
+                    if (index === currentPage - 1) applyStyle.push('active')
+                    return (
+                        <a className={applyStyle.join(" ")} key={number} onClick={() => paginate(number)} href='!#'>
+                            {number}
+                        </a>
+                    )
+                })}
+                <a href="#" onClick={() => paginate(pageNumbers.length)}>&raquo;</a>
+            </div>
+        </nav>
+    );
+};
--- src/containers/Restaurants.js
+++ src/containers/Restaurants.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 
+import { Pagination } from '../components/Pagination/Pagination';
 import RestaurantList from '../components/RestaurantList/RestaurantList';
 import { connect } from 'react-redux';
 import { restaurantApiActions } from '../store/actions/restaurants';
@@ -7,15 +8,42 @@ import { restaurantApiActions } from '../store/actions/restaurants';
 class RestaurantsContainer extends Component {
     constructor(props) {
         super(props);
+        this.state = {
+            searchFilter: '',
+            gridData: [],
+            currentPage: 1,
+            dataPerPage: 10
+        };
     }
 
     componentDidMount() {
         this.props.setRestaurants();
     }
 
+    paginate = pageNumber => {
+        this.setState({ currentPage: pageNumber })
+    }
+
+    componentWillReceiveProps(nextProps) {
+        this.setState({ gridData: nextProps.restaurants })
+    }
+
     render() {
+        const { currentPage, dataPerPage } = this.state;
+        const indexOfLastPost = currentPage * dataPerPage;
+        const indexOfFirstPost = indexOfLastPost - dataPerPage;
+        const currentGridData = this.state.gridData.slice(indexOfFirstPost, indexOfLastPost);
+
         return (
-            <RestaurantList restaurants={this.props.restaurants}/>
+            <>
+                <RestaurantList restaurants={currentGridData} />
+                <Pagination
+                    dataPerPage={dataPerPage}
+                    totaldata={this.state.gridData.length}
+                    paginate={this.paginate}
+                    currentPage={currentPage}
+                />
+            </>
         )
     };
 }
